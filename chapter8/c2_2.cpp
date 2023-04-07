bb06151ccb63cb88fb4a6c4b0769d46bc289739f
Pluto <78743401+wuyzan@users.noreply.github.com>
2023-04-07 23:05:47 +0800

    chapter8

---
+++ chapter8/c2_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream> 
+
+using namespace std;
+
+int main()
+{
+    //ofstream ofs("c2_2.txt", fstream::out); //以out形式打开文件会丢弃已有数据
+    ofstream ofs("c2_2.txt", fstream::out | fstream::app); //模式为输出和追加
+
+    return 0;
+}
