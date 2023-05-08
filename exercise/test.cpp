ec4f590d98abad06cc2a4b135a0d33049550a230
wuyan <wuyan98303@buaa.edu.cn>
2023-05-08 08:46:43 +0800

    add comments

---
+++ exercise/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    
+    int a = 0, b = 10;
+    cout << "a = " << a << endl;
+
+    a += b;
+    cout << "a = " << a << endl;
+}
