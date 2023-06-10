31723146ae7da85e1bfd65955e939f16911c05a8
wuyan <wuyan98303@buaa.edu.cn>
2023-06-10 10:52:19 +0800

    20230610

--- exercise/20230609_右值引用.cpp
+++ exercise/20230609_右值引用.cpp
@@ -6,11 +6,16 @@ class Example
 {
     public:
         Example() { cout << "example constructor" << endl; }
-        Example(const Example *ex) { cout << "copy constructor" << endl; }
+        Example(const Example &ex) { cout << "copy constructor" << endl; }
         ~Example() { cout << "destructor" << endl; }
         Example operator+(Example &other)
         {
             cout << "operator +" << endl;
+            return Example(); //默认构造函数
+        }
+        Example test() //返回类型为非引用类型的函数产生右值，右值向左值赋值完毕之后会调用析构函数
+        {
+            cout << "test function" << endl;
             return Example();
         }
 };
@@ -18,17 +23,26 @@ class Example
 class Another
 {
     public:
-        Another(const Example &ex) : example(ex) {  }
+        // Another(const Example &ex) {  } //对成员变量example调用默认构造函数
+        
+        Another(const Example &ex) : example(ex) {  } 
+        // 对成员变量example调用拷贝构造函数，在完成初始化之后，右值将消亡，因此会调用析构函数
     private:
-        Example example; 
+        Example example; //注意本类的对象析构时也会调用成员变量类对象的析构函数
 };
 
 
 int main()
 {
-    Example ex;
-    Another at(ex + ex); 
-    // 类Another的构造函数必须是const类型，因为算术表达式是右值，const的左值引用才可以绑定到右值
+    Example ex; //默认构造函数
+    // Another at(ex + ex); //加法运算符重载函数
+    // // 类Another的构造函数必须是const类型，因为算术表达式是右值，const的左值引用才可以绑定到右值
+
+    // ex = ex + ex;
+    // // 在给左值赋值完毕之后，调用加法运算符重载函数产生的临时右值也会消亡，因此也会调用析构函数
+
+    // Example ex1 = ex.test();
+    Example ex2 = ex;
 
     return 0;
 }
