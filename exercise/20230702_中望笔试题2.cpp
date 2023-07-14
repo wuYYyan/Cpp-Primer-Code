28b1c0839faee4d1f3097071050d25ab9e0d40af
wuyan <wuyan98303@buaa.edu.cn>
2023-07-14 17:43:03 +0800

    20230714

--- exercise/20230702_中望笔试题2.cpp
+++ exercise/20230702_中望笔试题2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     const char* p = "abc"; //字符串常量存放在只读段的文字常量区
-    cout << p << endl; 
+    // cout << p << endl; 
     char* c = (char*)p;  
     // c[1] = 'g'; 
     cout << p << endl;
