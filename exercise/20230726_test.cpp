5e206d5c8595a6aa8683adc2c30f81c80985178f
wuyan <wuyan98303@buaa.edu.cn>
2023-07-27 19:39:02 +0800

    20230727

---
+++ exercise/20230726_test.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int a = 0;
+    int j = 0;
+    int m = 0;
+    while ( m <= n ){
+        a = a + 1; 
+        for (int b = 1 ; b <= a ; b++){
+             j = j + a;
+             m = m + 1;
+        }
+    }
+        if (m != n)
+            j = j - ( m - n ) * a;
+    cout << j;
+    return 0;
+
+    int sum = 0;
+    int k = 1;
+    while (n >= 0)
+    {
+        n -= k; // n = n - k;
+        k += 1; // k = k + 1;
+        sum = sum + ;
+    }
+}
