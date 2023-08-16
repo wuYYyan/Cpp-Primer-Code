3cef46b9a603ec66f29993f72860c3d36a8cb8af
wuyan <wuyan98303@buaa.edu.cn>
2023-08-16 17:09:35 +0800

    20230816

--- exercise/test.cpp
+++ exercise/test.cpp
@@ -3,34 +3,11 @@
 
 using namespace std;
 
-class A
-{
-    public:
-        void show()
-        {
-            cout << "a = " << a << endl;
-        }
-
-    private:
-        int a;
-};
-
 int main()
 {
-    cout << "Hello World!" << endl;
+    int *p1;
     
-    A a; //默认构造函数对提供类内初始值的成员变量进行值初始化
-    a.show();
-
-    // int a = 0, b = 10;
-    // cout << "a = " << a << endl;
-
-    // a += b;
-    // cout << "a = " << a << endl;
-
-    // int i = 1;
-    // int &j = i;
-    // int &k = j;
-
+    int m[5][5] = {{1, 1, 1, 1, 1}, {2, 2, 2, 2, 2}};
+    int (*p2)[5] = m + 3;
     return 0;
 }
