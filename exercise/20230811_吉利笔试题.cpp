40ce459f01dda30a8ebc47ce81151d9702fbe8aa
wuyan <wuyan98303@buaa.edu.cn>
2023-08-16 22:57:29 +0800

    20230816

---
+++ exercise/20230811_吉利笔试题.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 选择题1
+    int k = 7 >> 1;
+    cout << "k = " << k << endl; // k = 3
+
+    // 选择题2
+    int j;
+    int a = (j = 3, j ++); // 只要在一个表达式中前置运算和后置运算就不一样，这是个逗号表达式
+    cout << "a = " << a << endl; // a = 3
+
+    // 选择题3
+    int arr[3] = {1, 2, 3};
+    cout << a << endl;
+    cout << *arr << endl;
+    cout << *(arr + 1) << endl;
+    cout << arr[0] << endl;
+    // cout << arr ++ << endl; // 表达式必须是可修改的左值，数组名是 const 常量
+
+    // 选择题4
+    char ch[] = "abc\0def";
+    char *p = ch;
+    printf("%c", *p + 4); // e
+    
+
+    return 0;
+}
