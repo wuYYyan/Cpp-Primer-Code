fd944753db81f5eeda6c0b86e802164a82676293
wuyan <wuyan98303@buaa.edu.cn>
2023-12-04 21:28:15 +0800

    20231204

---
+++ exercise/20231204_智能指针关联关键指针以及删除器的使用/20231204_智能指针关联关键指针以及删除器的使用.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+void close(FILE *fp) {
+    if (fp == nullptr)
+        return;
+    else
+        fclose(fp);
+
+    cout << "close file" << endl; // 用于验证删除器是否执行
+}
+
+int main() {
+    FILE *fp = fopen("word.txt", "w"); // 以只读方式打开文件
+    shared_ptr<FILE> file_ptr(fp, close); // 用shared_ptr管理fp, 并指定删除器为close
+    // 智能指针关联文件指针fp，当智能指针销毁时，会自动调用fclose()关闭文件
+}
